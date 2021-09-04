b7820976cfdd3c43ebb9d4d241c7e5a6b471d836
harry-martin <up896106@myport.ac.uk>
2021-09-05 00:34:06 +0100

    add basic file io and string functions

--- hlang.c
+++ hlang.c
@@ -1,7 +1,85 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+void display_usage()
 {
-    printf("hello, world!\n");
+    printf("Usage: hi <path>\n");
+}
+
+const char *get_source(const char *path)
+{
+    char *src = NULL;
+    FILE *f = fopen(path, "r");
+    if (f)
+    {
+        /* get the size of the file */
+        size_t src_size;
+        fseek(f, 0, SEEK_END);
+        src_size = ftell(f);
+        rewind(f);
+
+        /* read file into src */
+        src = malloc(src_size + 1);
+        fread(src, 1, src_size, f);
+        src[src_size] = '\0';
+    }
+    else
+    {
+        fprintf(stderr, "ERROR: failed to open file: %s\n", path);
+    }
+    return src;
+}
+
+bool char_in_string(const char c, const char *string)
+{
+    size_t string_index = 0;
+    while (string[string_index] != '\0')
+    {
+        if (string[string_index] == c)
+        {
+            return true;
+        }
+        ++string_index;
+    }
+    return false;
+}
+
+char *substr_by_delim(const char *src, char **dest, const char *delims)
+{
+    size_t char_count = 1;
+    char *offset = (char *)src;
+    while (!char_in_string(*offset, delims))
+    {
+        ++offset;
+        ++char_count;
+    }
+    *dest = (char *)malloc(char_count);
+    memcpy(*dest, src, char_count);
+    return ++offset;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 2)
+    {
+        const char *src = get_source(argv[1]);
+
+        char *word;
+        char *rest = (char *)src;
+        do{
+        rest = substr_by_delim(rest, &word, " ");
+        printf("%s\n", word);
+        } while (*word != '\0' && *rest != '\0');
+        return 0;
+    }
+    else
+    {
+        printf("ERROR: hi expects 1 argument but recieved %d\n", argc - 1);
+        display_usage();
+        return -1;
+    }
+
     return 0;
 }
